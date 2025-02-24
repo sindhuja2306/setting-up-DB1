d0a460fecaab5d1475a9cf56bd59a96d093a8fd7
paila sindhuja <sindhujapaila102@gmail.com>
2025-02-24 12:44:53 +0530

    DB

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to database');
+})
+.catch((error) => {
+    console.error(`Error connecting to database: ${error.message}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/', (req, res) => {
+    res.send('Customer Management System Backend');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
